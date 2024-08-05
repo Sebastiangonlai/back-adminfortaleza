8bf4767babf9cc5c18607973ab434957f0a79ab3
Sebastiangonlai <58099324+Sebastiangonlai@users.noreply.github.com>
2024-08-05 03:39:25 -0300

    sse

--- src/main/java/backend/academia/fortaleza/Application.java
+++ src/main/java/backend/academia/fortaleza/Application.java
@@ -2,17 +2,47 @@ package backend.academia.fortaleza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
         abrirNavegadorConRuntime("https://back-adminfortaleza-production.up.railway.app/swagger-ui/index.html");
+
         //abrirNavegadorConRuntime("http://localhost:8080/swagger-ui/index.html");
         //abrirNavegadorConRuntime("https://studyhub-backend-production.up.railway.app/swagger-ui/index.html");
     }
 
+  /*  @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }*/
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
     private static void abrirNavegadorConRuntime(String url) {
         try {
             Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
